4b682dcdd9579918ba89208bc8f2418974e38cb1
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-11-08 10:59:10 +0800

    Create QLineEditEchoModel.py

---
+++ controls/QLineEditEchoModel.py
@@ -0,0 +1,46 @@
+'''
+    QLineEdit控件
+
+    1.normal
+    2.noECHO
+    3.PASSWORD
+    4.passwordEchoonEdit
+
+
+
+
+'''
+from PyQt5.QtWidgets import *
+import sys
+
+class QLineEditEchoMode(QWidget):
+    def __init__(self):
+        super(QLineEditEchoMode, self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle("文本输入框的回显模式")
+        formLayout=QFormLayout()
+        normalLineEdit=QLineEdit()
+        noEchoLineEdit=QLineEdit()
+        passwordLineEdit=QLineEdit()
+        passwordEchoOnEditLineEdit=QLineEdit()
+        formLayout.addRow("Normal",normalLineEdit)
+        formLayout.addRow("NoEcho",noEchoLineEdit)
+        formLayout.addRow("Password",passwordLineEdit)
+        formLayout.addRow("PasswordEchoOnEdit",passwordEchoOnEditLineEdit)
+        # placeholdertext
+        normalLineEdit.setPlaceholderText("Normal")
+        noEchoLineEdit.setPlaceholderText("NoEcho")
+        passwordLineEdit.setPlaceholderText("password")
+        passwordEchoOnEditLineEdit.setPlaceholderText("PasswordEchoOnEdit")
+
+        normalLineEdit.setEchoMode(QLineEdit.Normal)
+        noEchoLineEdit.setEchoMode(QLineEdit.NoEcho)
+        passwordLineEdit.setEchoMode(QLineEdit.Password)
+        passwordEchoOnEditLineEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+        self.setLayout(formLayout)
+if __name__ == '__main__':
+    app=QApplication(sys.argv)
+    main=QLineEditEchoMode()
+    main.show()
+    sys.exit(app.exec_())
